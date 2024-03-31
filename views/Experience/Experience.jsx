a7bde9552cb17c00fc5c3209a4203fc34dd80dae
Jay-Sparks <jay_sparks@icloud.com>
2024-03-31 14:25:17 +0100

    adding 3d text

--- views/Experience/Experience.jsx
+++ views/Experience/Experience.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import * as THREE from 'three'
-import {OrbitControls, Sky, Sparkles, Sphere, Shadow, Billboard, Cloud, Clouds} from '@react-three/drei'
+import {OrbitControls, Sky, Sparkles, Sphere, Shadow, Billboard, Cloud, Clouds, Text3D, Center} from '@react-three/drei'
 
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Leva, useControls } from 'leva';
+
+import styles from './Experience.module.css'
 
 function Experience({isDark}) {
     const [ sunPosition, setSunPosition ] = useState([1, 2, 3])
@@ -26,53 +28,74 @@ function Experience({isDark}) {
       )
 
     return (
-        <Canvas camera={{ fov: 65, position: [0, 5, 20] }}>
-            <Sky sunPosition={sunPosition} />
-            {isDark ? 
-            <>
-                <hemisphereLight intensity={0.5} color="white" groundColor="black" />
-                <Sparkles count={60} scale={10 * 2} size={6} speed={0.4} />
-                <Moon color="white" amount={20} emissive="grey" size={0.8} position={[-3, 10, -2]} />
-                <Clouds material={THREE.MeshBasicMaterial}>
+        <>
+            <Canvas camera={{ fov: 65, position: [0, 5, 20] }}>
+                <Sky sunPosition={sunPosition} />
+                {isDark ? 
+                <>
+                    <hemisphereLight intensity={0.5} color="#c7c7c7" groundColor="black" />
+                    <Sparkles count={60} scale={10 * 2} size={6} speed={0.4} />
+                    <Moon color="white" amount={20} emissive="grey" size={0.8} position={[-3, 10, -2]} />
+                    <Clouds material={THREE.MeshBasicMaterial}>
+                        <Cloud seed={1} scale={1} volume={3} color="#c7c7c7" fade={100} position={[4, 10, 4]} />
+                        <Cloud seed={1} scale={1} volume={3} color="#white" fade={100} position={[-4, 10, -4]} />
+                    </Clouds>
+                </>
+                    :
+                    <Clouds material={THREE.MeshBasicMaterial}>
+                        <Cloud segments={40} bounds={[1, 0.5, 5]} volume={10} fade={60} color="white" position={[4, 7.5, 4]} scale={0.5}/>
+                        <Cloud segments={50} bounds={[5, 0.5, 2]} volume={10} fade={60} color="white" position={[3, 9, -5]} scale={0.5}/>
+                        <Cloud segments={40} bounds={[4, 1, 3]} volume={10} fade={60} color="white" position={[-5, 7, 2]} scale={0.5}/>
+                        <Cloud seed={1} scale={1} volume={3} color="#white" fade={100} position={[-4, 10, -4]} />
+                    </Clouds>
+                }
+                <ambientLight intensity={Math.PI / 8} />
+                <directionalLight
+                castShadow
+                intensity={2.5}
+                position={sunPosition}
+                shadow-normalBias={0.04}
+                />
+                <OrbitControls 
+                    target={[0, 1, 0]}
+                    minDistance={20}
+                    maxDistance={20}
+                    enableRotate={true}
+                    maxPolarAngle={Math.PI / 2}
+                    minPolarAngle={Math.PI / 6}
+                    
+                />
+                {/* <mesh castShadow position-y={ 0.75 }>
+                    <boxGeometry />
+                    <meshStandardMaterial />
+                </mesh> */}
+                <Center>
+                    <Text3D
+                        position={[0, 3, 0]}
+                        scale={[0.5, 0.5, 0.2]}
+                        font={"./helvetiker_regular.typeface.json"}
+                        curveSegments={24}
+                        bevelSegments={1}
+                        bevelEnabled
+                        bevelSize={0.08}
+                        bevelThickness={0.03}
+                        height={1}
+                        lineHeight={0.2}
+                        letterSpacing={0.2}
+                        castShadow
+                    >
+                    {`Jay Sparks`}
+                    <meshNormalMaterial color="white" />
+                </Text3D>
+                </Center>
 
-                    <Cloud seed={1} scale={1} volume={3} color="#c7c7c7" fade={100} position={[4, 10, 4]} />
-                    <Cloud seed={1} scale={1} volume={3} color="#white" fade={100} position={[-4, 10, -4]} />
-                </Clouds>
-            </>
-                :
-                <Clouds material={THREE.MeshBasicMaterial}>
-                    <Cloud segments={40} bounds={[1, 0.5, 5]} volume={10} fade={60} color="white" position={[4, 7.5, 4]} scale={0.5}/>
-                    <Cloud segments={50} bounds={[5, 0.5, 2]} volume={10} fade={60} color="white" position={[3, 9, -5]} scale={0.5}/>
-                    <Cloud segments={40} bounds={[4, 1, 3]} volume={10} fade={60} color="white" position={[-5, 7, 2]} scale={0.5}/>
-                    <Cloud seed={1} scale={1} volume={3} color="#white" fade={100} position={[-4, 10, -4]} />
-                </Clouds>
-            }
-            <ambientLight intensity={Math.PI / 8} />
-            <directionalLight
-              castShadow
-              intensity={2.5}
-              position={sunPosition}
-              shadow-normalBias={0.04}
-            />
-            <OrbitControls 
-                target={[0, 1, 0]}
-                minDistance={15}
-                maxDistance={20}
-                enableRotate={true}
-                maxPolarAngle={Math.PI / 2}
-                minPolarAngle={Math.PI / 6}
-                
-            />
-            <mesh castShadow position-y={ 0.75 }>
-                <boxGeometry />
-                <meshStandardMaterial />
-            </mesh>
-
-            <mesh receiveShadow position-y={ -0.3 } >
-                <boxGeometry args={ [ 10, 0.5, 10 ] } />
-                <meshStandardMaterial color="#8f4111" />
-            </mesh>
-        </Canvas>
+                <mesh receiveShadow position-y={ -0.3 } >
+                    <boxGeometry args={ [ 10, 0.5, 10 ] } />
+                    <meshStandardMaterial color="#8f4111" />
+                </mesh>
+            </Canvas>
+            <h1>Creative Web Developer with over 8 years experience</h1>
+        </>
     )
 }
 
