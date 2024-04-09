f7e45c9c0d902ef57730a6e1feacacde3690b887
Jay-Sparks <jay_sparks@icloud.com>
2024-04-09 22:22:13 +0100

    build for testing

--- Menu/Menu.jsx
+++ Menu/Menu.jsx
@@ -1,30 +1,29 @@
-import React from 'react'
+import React from 'react';
 
-import styles from './Menu.module.css'
-import NavBar from '../components/NavBar/NavBar'
-import { CgMenuLeft } from 'react-icons/cg'
+import styles from './Menu.module.css';
+import NavBar from '../components/NavBar/NavBar';
+import { CgMenuLeft } from 'react-icons/cg';
 
 function Menu({ isMenu, setIsMenu }) {
   return (
     <>
-        {isMenu ? 
-            <div className={styles.menuWrapper}>
-                <div className={styles.sideBar1}>
-                    <NavBar />
-                </div>
-                <div className={styles.cover} onClick={() => setIsMenu(false)}></div>
-            </div>
-        : 
-            <button className="menu" onClick={() => setIsMenu(true)}>
-                <CgMenuLeft className={styles.menuIcon} />
-            </button>
-        }
-
-        <div className={styles.sideBar}>
+      {isMenu ? (
+        <div className={styles.menuWrapper}>
+          <div className={styles.sideBar1}>
             <NavBar />
+          </div>
+          <div className={styles.cover} onClick={() => setIsMenu(false)}></div>
         </div>
+      ) : (
+        <button className="menu" onClick={() => setIsMenu(true)}>
+          <CgMenuLeft className={styles.menuIcon} />
+        </button>
+      )}
+      <div className={styles.sideBar}>
+        <NavBar />
+      </div>
     </>
-  )
+  );
 }
 
-export default Menu
+export default Menu;
--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
-import { useState } from 'react'
-import { HiOutlineMenuAlt2 } from "react-icons/hi";
-import { CgMenuLeft } from "react-icons/cg";
+import { useState } from 'react';
 
-import NavBar from '../components/NavBar/NavBar'
-import Toggle from '../components/Toggle/Toggle'
-import Experience from '../views/Experience/Experience'
-import Projects from '../components/Projects/Projects'
-
-import './App.css'
+import './App.css';
 import Menu from '../Menu/Menu';
 import Home from '../views/Home/Home';
 
 export const App = () => {
-  const [ isDark, setIsDark ] = useState(false)
-  const [ isMenu, setIsMenu ] = useState(false)
+  const [isDark, setIsDark] = useState(false);
+  const [isMenu, setIsMenu] = useState(false);
 
   console.log(isMenu);
 
   return (
-    <div className="App" data-theme={isDark ? "dark" : "light"}>
-      <Menu isMenu={isMenu} setIsMenu={setIsMenu}/>
-      <Home isDark={isDark} setIsDark={setIsDark}/>
+    <div className="App" data-theme={isDark ? 'dark' : 'light'}>
+      <Menu isMenu={isMenu} setIsMenu={setIsMenu} />
+      <Home isDark={isDark} setIsDark={setIsDark} />
     </div>
-  )
-}
+  );
+};
